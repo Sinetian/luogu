95be5ee4dca753086b72fb087428a17d4d62ce1f
Sinetian <liujingyu2020@outlook.com>
2023-08-12 16:51:27 +0800

    code

--- self-code/高精度加减乘除.cpp
+++ self-code/高精度加减乘除.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 using namespace std;
-int a[114514], b[114514], c[114514];
-//加
+int a[1100], b[1100], c[1100];
+// 读入
 void readBIG(string s, int a[])
 {
     int len = s.size();
-    for(int i = 1; i <= len; i++) a[i] = s[len - i] - '0';
+    for (int i = 1; i <= len; i++)
+        a[i] = s[len - i] - '0'; // 从高位开始读入
     a[0] = len;
 }
+// 输出
 void printBIG(int a[])
 {
-    int len = a[0];
-    for(int i = len; i >= 1; i--) cout << a[i];
+    for (int i = a[0]; i >= 1; i--)
+        cout << a[i]; // 因为是反向读入，所以得反过来输出
     cout << endl;
 }
+// 加法
 void addBIG(int a[], int b[], int c[])
 {
     c[0] = max(a[0], b[0]);
-    int len = c[0];
-    for(int i = 1; i <= len; i++) c[i] = a[i] + b[i];
-    for(int i = 1; i <= len; i++)
+    for(int i = 1; i <= c[0]; i++) c[i] = a[i] + b[i];
+    for(int i = 1; i <= c[0]; i++)
     {
         if(c[i] >= 10)
         {
             c[i + 1] += c[i] / 10;
             c[i] %= 10;
         }
-        if(c[len + 1] > 0) len++;
+        if(c[c[0] + 1] > 0) c[0]++;
     }
-    c[0] = len;
 }
-//减
+// 减法比较，返回较小值
 bool cmpBIG(int a[], int b[])
 {
-	int la = a[0], lb = b[0];
-	if(la != lb) return la < lb;
-	for(int i = la; i >= 1; i--)
-	{
-		if(a[i] != b[i]) return a[i] < b[i];
-	}
-	return false;
+    int la = a[0], lb = b[0];
+    if (la != lb)
+        return la < lb;
+    for (int i = la; i >= 1; i--)
+    {
+        if (a[i] != b[i])
+            return a[i] < b[i];
+    }
+    return false;
 }
+// 减法
 void subBIG(int a[], int b[], int c[])
 {
 	c[0] = a[0];
@@ -54,13 +58,13 @@ void subBIG(int a[], int b[], int c[])
 			c[i + 1]--;
 		}
 	}
-	while(c[len] == 0 && len > 1) len--;
+	while(c[len] == 0 && len > 1) len--; // 删除前导0
 	c[0] = len;
 }
-//乘
+//乘法(高 * 单)
 void mulBIG(int a[], int b, int c[])
 {
-	c[0] = a[0];
+    c[0] = a[0];
 	int len = c[0];
 	for(int i = 1; i <= len; i++) c[i] = a[i] * b;
 	for(int i = 1; i <= len; i++)
@@ -81,25 +85,25 @@ void mulBIG(int a[], int b, int c[])
 		c[0]++;
 	}
 }
-//除
-void divBIG(int a[], int b, int c[])
+//除法(高 / 单)
+void divBIG(int a[], int b[], int c[])
 {
-	c[0] = a[0];
-	int len = a[0];
-	int r = 0;
-	for(int i = len; i >= 1; i--)
+    c[0] = a[0];
+    int len = c[0], r = 0;
+    for(int i = len; i >= 1; i--)
 	{
 		int t = r * 10 + a[i]; // 重点：必须备份
-		c[i] = t / b;
-		r = t % b;
+		c[i] = t / b[i];
+		r = t % b[i];
 	}
-	while(c[c[0]] == 0 && c[0] > 1) c[0]--;
 }
 int main()
 {
-    string s;
-    int x;
-    cin >> s;
-
+    string s1, s2;
+    cin >> s1 >> s2;
+    readBIG(s1, a);
+    readBIG(s2, b);
+    addBIG(a, b, c);
+    printBIG(c);
     return 0;
 }
