f71903088d8346f3b7a99524a97bda30c76f580a
Sinetian <liujingyu2020@outlook.com>
2023-08-12 12:53:26 +0800

    code changes

---
+++ luogu-original/B2029.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+const double Pi = 3.14159;
+int main()
+{
+    int r, h, v, ans;
+    cin >> h >> r;
+    v = Pi * r * r * h;
+    if (20000 % (int)v != 0)
+        ans = 20000 / (int)v + 1;
+    else
+        ans = 20000 / (int)v;
+    cout << ans << endl;
+    return 0;
+}
